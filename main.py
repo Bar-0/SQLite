5ffb216aa7d6996230dde9869356b5b6ce767eb3
Bar0 <Aleh.Kornach@gmail.com>
2023-11-22 23:48:39 +0300

    create database

--- main.py
+++ main.py
@@ -2,5 +2,12 @@ def say_hello(a):
     print('Hello', a, '!')
 name = 'Oleg'
 say_hello(name)
-name2 = 'Maxim'
+name2 = 'Alexander'
 say_hello(name2)
+
+import sqlite3
+
+# Создаем подключение к базе данных (файл my_database.db будет создан)
+connection = sqlite3.connect('my_phonedatabase.db')
+
+connection.close()
